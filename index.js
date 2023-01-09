22fbed14aa20d80563c253246a6690fbd659161d
manu-andaluz <manupeandaluz@gmail.com>
2023-01-08 23:09:47 -0300

    changing port number

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 });
 
 const uri = process.env.DB_URI
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}...`);
