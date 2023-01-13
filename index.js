80fe31cc7ed17326c41c282c9d050d95356bdae6
manu-andaluz <manupeandaluz@gmail.com>
2023-01-13 00:25:26 -0300

    adding new routes

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const register = require("./routes/register");
 const login = require("./routes/login");
 const orders = require("./routes/orders");
 const stripe = require("./routes/stripe");
-const productsRoute = require("./routes/products");
+const products = require("./routes/products");
+const users = require('./routes/users')
 
 const app = express();
 
@@ -25,14 +26,19 @@ app.use("/api/register", register);
 app.use("/api/login", login);
 app.use("/api/orders", orders);
 app.use("/api/stripe", stripe);
-app.use("/api/products", productsRoute);
+app.use("/api/products", products);
+app.use("/api/users", users);
 
 app.get("/", (req, res) => {
   res.send("Welcome our to online shop API...");
 });
 
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+
 const uri = process.env.DB_URI
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}...`);
